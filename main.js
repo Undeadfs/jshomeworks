1f8060cfcef9d7b3787f9660d3180009d9f7fadb
Ruslan <impsermy@gmail.com>
2022-06-01 22:25:01 +0400

    2nd homeworks tasks implemented

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 function getSum(a,b) {
 
     return a + b;
-    
+
 }
 
 // 2. Show even numbers between a and b
@@ -11,7 +11,7 @@ function getSum(a,b) {
 function showEvenNumbers(a,b) {
 
     for(let n = a ; n <= b ; n++) {
-        if ( n % 2 == 0) {
+        if ( n % 2 === 0) {
             console.log(n)
         }
     }
@@ -23,11 +23,36 @@ function showEvenNumbers(a,b) {
 function showLargestNumber(a,b,c) {
 
     if (a > c && c > b) {
+
 		console.log(a + ' is the largest number');
+
 	} else if (b > a && b > c) {
+
 		console.log(b + ' is the largest number');
+        
 	} else {
+
 		console.log(c + ' is the largest number');
 	}
 
 }
+
+// 4. isEmpty function for checking object properties
+
+let objectWithProp = {a:1};
+let emptyObject = {};
+
+function isEmpty(obj) {
+    return Object.keys(obj).length === 0;
+} 
+
+console.log(isEmpty(objectWithProp));
+
+// 5. Show firstName and lastName
+
+let avenger = {
+    firstName : 'Tony',
+    lastName : 'Stark'
+}
+
+console.log(`Avenger's name is : ${avenger.firstName} ${avenger.lastName}`);
